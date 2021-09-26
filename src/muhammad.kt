593eaff4f2ecf77a79a315cf7ae614f056b2ec9d
Muhammad BBassem <mohamedbasim55@gmail.com>
2021-09-26 19:35:08 +0300

    Initial commit

---
+++ src/muhammad.kt
@@ -0,0 +1,17 @@
+fun like(oldLikesCount:Int):Int=oldLikesCount+1
+fun dislike(oldLikesCount: Int):Int=oldLikesCount-1
+fun getLikesDetails(likesCount:Int):String="Likes: $likesCount"
+fun showLikes(likesCount: Int)= println(getLikesDetails(likesCount))
+fun sayHello(name:String="Friend")="Hello $name"
+fun showSayHelloMessage(name: String)= print(sayHello(name))
+fun check(names:)
+fun main(){
+    var checker:Boolean=false
+    var names= listOf("Muhammad","Bassem")
+    var inputName:String= readLine()!!
+    for (element in names)
+        if (element==inputName) checker=true
+    print(if (checker) "found" else "not found")
+}
+
+
