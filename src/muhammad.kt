46a6c01be6839229a72faa96e75da950c6714705
mucisco <82220978+mucisco@users.noreply.github.com>
2021-09-27 09:45:05 +0300

    add greater function

--- src/muhammad.kt
+++ src/muhammad.kt
@@ -1,6 +1,6 @@
 fun isEven(inputNumber:Int)=inputNumber%2==0
 fun isPositive(inputNumber: Int)=inputNumber>=0
-
+fun isGreater(inputNumber: Int,secondInputNumber:Int)=inputNumber>secondInputNumber
 fun main(){
     var number=8
     println(isEven(number))
