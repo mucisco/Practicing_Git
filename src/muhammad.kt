144cec9deb8cf19d13e0a556a9f2855d3ea8f8c4
mucisco <82220978+mucisco@users.noreply.github.com>
2021-09-27 10:10:34 +0300

    add filter function for a list

--- src/muhammad.kt
+++ src/muhammad.kt
@@ -2,6 +2,13 @@ fun isEven(inputNumber:Int)=inputNumber%2==0
 fun isPositive(inputNumber: Int)=inputNumber>=0
 fun isGreater(inputNumber: Int,secondInputNumber:Int)=inputNumber>secondInputNumber
 fun getFriendsDetails(friendsList:List<String>)="You have ${friendsList.size} friends"
+fun getNameStartWith(names:List<String>,firstChar:Char):List<String>{
+     var filteredList= mutableListOf<String>()
+    for (firend in names)
+        if(firend.get(0)==firstChar)
+            filteredList.add(firend)
+    return filteredList
+}
 fun main(){
     var friends= mutableListOf("Mustafa","Ahmed","Ali")
     print(getFriendsDetails(friends))
