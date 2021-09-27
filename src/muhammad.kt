c0e07bf556b1fa87d7ce64142dde874f58d96385
mucisco <82220978+mucisco@users.noreply.github.com>
2021-09-27 09:50:00 +0300

    add list as parameter

--- src/muhammad.kt
+++ src/muhammad.kt
@@ -1,9 +1,10 @@
 fun isEven(inputNumber:Int)=inputNumber%2==0
 fun isPositive(inputNumber: Int)=inputNumber>=0
 fun isGreater(inputNumber: Int,secondInputNumber:Int)=inputNumber>secondInputNumber
+fun getFriendsDetails(friendsList:List<String>)="You have ${friendsList.size} friends"
 fun main(){
-    var number=8
-    println(isEven(number))
+    var friends= mutableListOf("Mustafa","Ahmed","Ali")
+    print(getFriendsDetails(friends))
 
 }
 
