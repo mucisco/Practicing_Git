71af31c473ac5b85f288bec73ba4406ed9c3be04
Muhammad BBassem <mohamedbasim55@gmail.com>
2021-09-27 09:21:25 +0300

    write even function

--- src/muhammad.kt
+++ src/muhammad.kt
@@ -1,17 +1,8 @@
-fun like(oldLikesCount:Int):Int=oldLikesCount+1
-fun dislike(oldLikesCount: Int):Int=oldLikesCount-1
-fun getLikesDetails(likesCount:Int):String="Likes: $likesCount"
-fun showLikes(likesCount: Int)= println(getLikesDetails(likesCount))
-fun sayHello(name:String="Friend")="Hello $name"
-fun showSayHelloMessage(name: String)= print(sayHello(name))
+fun isEven(inputNumber:Int)=if(inputNumber%2==0) true else false
 fun main(){
-    
-    var names= listOf("Muhammad","Bassem")
+    var number=8
+    println(isEven(number))
 
-    var inputName:String= readLine()!!
-    for (element in names)
-        if (element==inputName) checker=true
-    print(if (checker) "found" else "not found")
 }
 
 
