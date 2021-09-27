49e1d760f2b2a8c79cedfc222d32e42f613f43a2
mucisco <82220978+mucisco@users.noreply.github.com>
2021-09-27 09:39:16 +0300

    no changes

--- src/muhammad.kt
+++ src/muhammad.kt
@@ -1,5 +1,6 @@
 fun isEven(inputNumber:Int)=if(inputNumber%2==0) true else false
 fun isPositive(inputNumber: Int)=if(inputNumber>=0) true else false
+
 fun main(){
     var number=8
     println(isEven(number))
