6d0019dbbff1bdd52eba8763560a7e382ae40e66
mucisco <82220978+mucisco@users.noreply.github.com>
2021-09-27 11:22:46 +0300

    Update muhammad.kt

--- src/muhammad.kt
+++ src/muhammad.kt
@@ -1,5 +1,5 @@
 fun isEven(inputNumber:Int)=inputNumber%2==0 //this function used to know if the number is even or not
-fun isPositive(inputNumber: Int)=inputNumber>=0 //this function used to know if the number is even or not 
+fun isPositive(inputNumber: Int)=inputNumber>=0 //this function used to know if the number is positive or not 
 fun isGreater(inputNumber: Int,secondInputNumber:Int)=inputNumber>secondInputNumber //this function used to know if the 1st no. is greater than 2nd no,
 fun getFriendsDetails(friendsList:List<String>)="You have ${friendsList.size} friends" //this funtion used to display number of friends
 fun getNameStartWith(names:List<String>,firstChar:Char):List<String>{ //filter function
