f63a7e9f831b4fa68fa30efdbb51397ce8715784
mucisco <82220978+mucisco@users.noreply.github.com>
2021-09-27 11:33:34 +0300

    add comments

--- src/muhammad.kt
+++ src/muhammad.kt
@@ -1,8 +1,11 @@
+//region functions
 fun isEven(inputNumber:Int)=inputNumber%2==0 //this function used to know if the number is even or not
 fun isPositive(inputNumber: Int)=inputNumber>=0 //this function used to know if the number is positive or not
 fun isGreater(inputNumber: Int,secondInputNumber:Int)=inputNumber>secondInputNumber //this function used to know if the 1st no. is greater than 2nd no,
 fun getFriendsDetails(friendsList:List<String>)="You have ${friendsList.size} friends" //this funtion used to display number of friends
-fun getNameStartWith(names:List<String>,firstChar:Char):List<String>{ //filter function
+// endregion
+
+fun getNameStartWith(names:List<String>,firstChar:Char):List<String>{
      var filteredList= mutableListOf<String>()
     names.forEach(){
         if(it.get(0)=='A')
@@ -12,7 +15,6 @@ fun getNameStartWith(names:List<String>,firstChar:Char):List<String>{ //filter f
 }
 fun main(){
     var friends= mutableListOf("Mustafa","Ahmed","Ali")
- //test
 }
 
 
