3f7ec2d751729a030bd9b23c37c8953db3f1715f
Muhammad BBassem <mohamedbasim55@gmail.com>
2021-09-27 09:26:46 +0300

    write positive function

--- src/muhammad.kt
+++ src/muhammad.kt
@@ -1,4 +1,5 @@
 fun isEven(inputNumber:Int)=if(inputNumber%2==0) true else false
+fun isPositive(inputNumber: Int)=if(inputNumber>=0) true else false
 fun main(){
     var number=8
     println(isEven(number))
