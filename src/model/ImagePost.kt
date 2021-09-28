7a7ff48701efb21161b018eb97d4e8466cffeb53
mucisco <82220978+mucisco@users.noreply.github.com>
2021-09-28 16:32:28 +0300

    apply inheritance on ImagePost

--- src/model/ImagePost.kt
+++ src/model/ImagePost.kt
@@ -1,28 +1,7 @@
 package model
 
-class ImagePost {
-    var userName:String=""
-    var content=""
-
-    var likeCount=0
-        private set
-    var commentsCount=0
-        private set
-
-    var comments= mutableListOf<String>()
-
-    fun like(){
-        likeCount++
-        println("Have a new like")
-    }
-    fun addComment(comment:String){
-        comments.add(comment)
-        commentsCount++
-    }
+class ImagePost:Post() {
     var imageLink:String=""
-
-
-
 
 
 }
